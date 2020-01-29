5146de82b5fbcb41a3e855eb79a044ffd1a016d7
Davidodari <davidkibzodari@gmail.com>
2020-01-30 01:04:08 +0300

    rename annotation

---
+++ annotation/src/main/java/com/dari/annotation/Builder.kt
@@ -0,0 +1,10 @@
+package com.dari.annotation
+
+/**
+ * Created By David Odari
+ * On 30/11/19
+ *
+ **/
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS)
+annotation class Builder
